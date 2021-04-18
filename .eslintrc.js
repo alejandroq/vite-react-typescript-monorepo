8ff2f90a29de4ca82f7c01d2b9d6704e445c8807
alejandroq <quesadaaba@gmail.com>
2021-04-18 14:41:15 -0600

    added husky and lint-staged.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,28 +1,21 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true,
-        "node": true
+    env: {
+        browser: true,
+        es2021: true,
+        node: true,
     },
-    "extends": [
-        "plugin:react/recommended",
-        "standard",
-        "prettier"
-    ],
-    "parser": "@typescript-eslint/parser",
-    "parserOptions": {
-        "ecmaFeatures": {
-            "jsx": true
+    extends: ['plugin:react/recommended', 'standard', 'prettier'],
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        ecmaFeatures: {
+            jsx: true,
         },
-        "ecmaVersion": 12,
-        "sourceType": "module"
+        ecmaVersion: 12,
+        sourceType: 'module',
     },
-    "plugins": [
-        "react",
-        "@typescript-eslint"
-    ],
-    "rules": {
-        "no-use-before-define": "off",
-        "@typescript-eslint/no-use-before-define": ["error"]
-    }
+    plugins: ['react', '@typescript-eslint'],
+    rules: {
+        'no-use-before-define': 'off',
+        '@typescript-eslint/no-use-before-define': ['error'],
+    },
 };
